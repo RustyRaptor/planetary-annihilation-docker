9c0415d4b6d69490c5a99bae590efaebf5d72aa5
RustyRaptor <RustyRaptor@users.noreply.github.com>
2022-01-03 19:00:46 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,7 +42,6 @@ fi
 COMMAND="$COMMAND $@"
 
 log "Starting server with command: $COMMAND"
-log "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-/home/ziad/patserver/server     --allow-lan     --enable-crash-reporting     --mt-enabled     --output-dir /home/ziad/replays     --replay-filename UTCTIMESTAMP     --port 20545     --server-name HomeBoysUnited PA hangout     --max-players 24     --max-spectators 5     --spectators 5 --game-mode PAExpansion1:config --server-password e 
-log "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA2"
+
+exec $COMMAND
 
